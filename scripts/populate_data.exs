a508e4e2891c31c26365af8893c829a0df94c653
Yauheni Tsiarokhin <yauheni@tsiarokhin.me>
2020-06-20 15:52:17 +0300

    persist recipes

--- scripts/populate_data.exs
+++ scripts/populate_data.exs
@@ -13,6 +13,10 @@ defmodule Script do
     steps = DataLoader.load_recipe_steps!(data_dir, langs)
 
     :ok = DataBase.persist_recipe_steps!(conn, steps)
+
+    recipes = DataLoader.load_recipes!(data_dir)
+
+    :ok = DataBase.persist_recipes!(conn, recipes)
   end
 
 end
@@ -50,6 +54,54 @@ defmodule DataBase do
         Enum.each(steps, fn(step) ->
           {:ok, _index} = Redix.command(conn, ["RPUSH", key, step])
         end)
+      end)
+    end)
+  end
+
+  def persist_recipes!(conn, recipes) do
+    Enum.each(recipes, fn({recipe_name, recipe}) ->
+      type_key = "recipe:#{recipe_name}:type"
+      {:ok, "OK"} = Redix.command(conn, ["SET", type_key, Map.fetch!(recipe, "type")])
+
+      ingredients_key = "recipe:#{recipe_name}:ingredients"
+
+      Logger.debug(
+        """
+        Persisting recipe ingrdients.
+        Recipe: #{recipe["name"]}.
+        Key: #{ingredients_key}
+        """
+      )
+
+      Enum.each(recipe["ingredients"], fn(ingredient) ->
+        ingredient_name = Map.fetch!(ingredient, "name")
+        {:ok, _index} = Redix.command(conn, ["RPUSH", ingredients_key, ingredient_name])
+
+        ingredient_key = "recipe:#{recipe_name}:ingredient:#{ingredient_name}"
+
+        Logger.debug(
+          """
+          Persisting recipe ingrdient.
+          Recipe: #{recipe["name"]}.
+          Ingredient: #{ingredient["name"]}.
+          Key: #{ingredient_key}
+          """
+        )
+
+        case Map.fetch!(ingredient, "quantity") do
+          "ANY" ->
+            {:ok, _index} = Redix.command(conn, [
+              "HSET", ingredient_key, "quantity", "ANY"
+            ])
+          _ ->
+            {:ok, "OK"} = Redix.command(conn, [
+              "HMSET", ingredient_key,
+              "quantity", Map.fetch!(ingredient, "quantity"),
+              "unit", Map.fetch!(ingredient, "unit"),
+              "min", Map.fetch!(ingredient, "min"),
+              "max", Map.fetch!(ingredient, "max")
+            ])
+        end
       end)
     end)
   end
